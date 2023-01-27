1ac035dd9c5208ca620dbee104e5c8a504cb308e
Ray Ortiz <oray@bgsu.edu>
2023-01-26 19:38:34 -0500

    Completed Games 1&2

--- scripts/win.js
+++ scripts/win.js
@@ -1,17 +1,21 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "";
-const contractName = "Game1";
+const gameAddr = "0xcf7ed3acca5a467e9e704c703e8d87f634fb0fc9";
+const contractName = "Game2";
 
 async function main() {
     // attach to the game
     const game = await hre.ethers.getContractAt(contractName, gameAddr);
 
     // do whatever you need to do to win the game here:
-    const tx = await game.win();
+    const tx1 = await game.setX(25);
+    await tx1.wait(); //waiting for the block to be mined
+    const tx2 = await game.setY(25);
+    await tx2.wait(); //waiting for the block to be mined
+    const tx3 = await game.win();
 
     // did you win? Check the transaction receipt!
     // if you did, it will be in both the logs and events array
-    const receipt = await tx.wait();
+    const receipt = await tx3.wait();
     console.log(receipt);
 }
 
