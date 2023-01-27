71097482db7799b6b042ac86e8c99508ab2f2a10
Ray Ortiz <oray@bgsu.edu>
2023-01-26 19:40:35 -0500

    Completed Game 3.

--- scripts/win.js
+++ scripts/win.js
@@ -1,27 +1,24 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "0xcf7ed3acca5a467e9e704c703e8d87f634fb0fc9";
-const contractName = "Game2";
+const gameAddr = "0xa51c1fc2f0d1a1b8494ed1fe312d7c3a78ed91c0";
+const contractName = "Game3";
 
 async function main() {
-    // attach to the game
-    const game = await hre.ethers.getContractAt(contractName, gameAddr);
+  // attach to the game
+  const game = await hre.ethers.getContractAt(contractName, gameAddr);
 
-    // do whatever you need to do to win the game here:
-    const tx1 = await game.setX(25);
-    await tx1.wait(); //waiting for the block to be mined
-    const tx2 = await game.setY(25);
-    await tx2.wait(); //waiting for the block to be mined
-    const tx3 = await game.win();
+  // do whatever you need to do to win the game here:
 
-    // did you win? Check the transaction receipt!
-    // if you did, it will be in both the logs and events array
-    const receipt = await tx3.wait();
-    console.log(receipt);
+  const tx = await game.win(45);
+
+  // did you win? Check the transaction receipt!
+  // if you did, it will be in both the logs and events array
+  const receipt = await tx.wait();
+  console.log(receipt);
 }
 
 main()
   .then(() => process.exit(0))
-  .catch(error => {
+  .catch((error) => {
     console.error(error);
     process.exit(1);
   });
