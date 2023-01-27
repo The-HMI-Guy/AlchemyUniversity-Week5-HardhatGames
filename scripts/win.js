dc4908d7dd58efdbf2497038193f88ff74ae72e8
Ray Ortiz <oray@bgsu.edu>
2023-01-27 07:38:05 -0500

    Game 4 completed and I add the other games

--- scripts/win.js
+++ scripts/win.js
@@ -1,21 +1,94 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "0xa51c1fc2f0d1a1b8494ed1fe312d7c3a78ed91c0";
-const contractName = "Game3";
+const gameAddr = "0x9a676e781a523b5d0c0e43731313a708cb607508";
+const contractName = "Game4";
 
 async function main() {
-  // attach to the game
-  const game = await hre.ethers.getContractAt(contractName, gameAddr);
+//    // attach to the game
+   const game = await hre.ethers.getContractAt(contractName, gameAddr)
 
-  // do whatever you need to do to win the game here:
 
-  const tx = await game.win(45);
+   const tx = await game.win(56)
 
-  // did you win? Check the transaction receipt!
-  // if you did, it will be in both the logs and events array
-  const receipt = await tx.wait();
-  console.log(receipt);
+//    // did you win? Check the transaction receipt!
+//    // if you did, it will be in both the logs and events array
+   const receipt = await tx.wait()
+   console.log(receipt)
 }
+////////////
+// Game 4
+//__________
 
+// async function main() {
+//    // attach to the game
+//    const game = await hre.ethers.getContractAt(contractName, gameAddr)
+
+//    // sum = 10 = x + y
+//    // y = 210
+//    // x = (210 + 45) + 1 + 10 overflow
+//    const tx = await game.win(56)
+
+//    // did you win? Check the transaction receipt!
+//    // if you did, it will be in both the logs and events array
+//    const receipt = await tx.wait()
+//    console.log(receipt)
+// }
+
+////////////
+// Game 3
+//__________
+
+// async function main() {
+//     // attach to the game
+//     const game = await hre.ethers.getContractAt(contractName, gameAddr);
+
+//     // do whatever you need to do to win the game here:
+//     const tx = await game.win(45);
+
+//     // did you win? Check the transaction receipt!
+//     // if you did, it will be in both the logs and events array
+//     const receipt = await tx.wait();
+//     console.log(receipt);
+// }
+
+////////////
+// Game 2
+//__________
+
+// async function main() {
+//     // attach to the game
+//     const game = await hre.ethers.getContractAt(contractName, gameAddr);
+
+//     // do whatever you need to do to win the game here:
+//     await game.setX(25);
+//     await game.setY(25);
+//     const tx = await game.win();
+
+//     // did you win? Check the transaction receipt!
+//     // if you did, it will be in both the logs and events array
+//     const receipt = await tx.wait();
+//     console.log(receipt);
+// }
+
+////////////
+// Game 1
+//__________
+
+// add the game address here and update the contract name if necessary
+// const gameAddr = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
+// const contractName = "Game1";
+
+// async function main() {
+//     // attach to the game
+//     const game = await hre.ethers.getContractAt(contractName, gameAddr);
+
+//     // do whatever you need to do to win the game here:
+//     const tx = await game.win();
+
+//     // did you win? Check the transaction receipt!
+//     // if you did, it will be in both the logs and events array
+//     const receipt = await tx.wait();
+//     console.log(receipt);
+// }
 main()
   .then(() => process.exit(0))
   .catch((error) => {
